f7a8a69262d53ced9ad9453c5e97f5bbc2f383ad
clubberstake <gregory.lang@gmail.com>
2015-06-25 19:24:33 -0400

    adding initial code from meetup 6-25-15

---
+++ OxfordFace/ColorImageFrame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OxfordFace
+{
+    class ColorImageFrame
+    {
+        public int PixelDataLength { get; set; }
+    }
+}
